399b9fbad45b8d2a209247a0c9a4e40985fca6d3
stratus-ss <steve.ovens@x86innovations.com>
2025-08-20 09:43:20 -0500

    attempting to fix duplicate entries

--- plugins/modules/pfsense_dns_resolver.py
+++ plugins/modules/pfsense_dns_resolver.py
@@ -597,15 +597,22 @@ class PFSenseDNSResolverModule(PFSenseModuleBase):
         if params["sslcert"] and not self.pfsense.find_cert_elt(params["sslcert"]):
             self.module.fail_json(msg=f'sslcert, {params["sslcert"]} is not a valid description of cert')
 
-        for host in params["hosts"]:
+        for host in params.get("hosts", []):
             for ipaddr in host["ip"].split(","):
                 if not self.pfsense.is_ipv4_address(ipaddr):
                     self.module.fail_json(msg=f'ip, {ipaddr} is not a ipv4 address')
 
-        if params["domainoverrides"] is not None:
-            for domain in params["domainoverrides"]:
-                if not self.pfsense.is_ipv4_address(domain["ip"]):
-                    self.module.fail_json(msg=f'ip, {domain["ip"]} is not a ipv4 address')
+        for domain in params.get("domainoverrides", []):
+            if not self.pfsense.is_ipv4_address(domain["ip"]):
+                self.module.fail_json(msg=f'ip, {domain["ip"]} is not a ipv4 address')
+
+        # Safely handle interface parameters that might be None
+        active_interfaces = params.get("active_interface") or ["all"]
+        outgoing_interfaces = params.get("outgoing_interface") or ["all"]
+        
+        for if_descr in active_interfaces + outgoing_interfaces:
+            if not self.pfsense.is_interface_display_name(if_descr) and if_descr.lower() != "all":
+                self.module.fail_json(msg=f'if_descr, {if_descr}, is not exist')
 
     ##############################
     # XML processing
